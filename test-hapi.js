665ccf7eeaca5dcc13f1e40970080bfef46328b3
Halbert Stone <halbertstone@gmail.com>
2014-12-27 13:20:51 -0800

    test-hapi 'Hello World' is working

---
+++ test-hapi.js
@@ -0,0 +1,19 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({ port: 3000 });
+ 
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
+
+// above only sets up the server
+// need to supply routes to direct handling of requests
+//
+server.route({ 
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('\nHello World!\ntest-hapi is alive!\n');
+  }
+});
